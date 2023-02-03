d0bbb91c728e7f2c1083d3d393a2e157feb59735
ludmilact <ludmilatimoteo@Outlook.com>
2023-02-03 15:07:45 -0300

    inicio da implementaçao tela de cadastro do usuario

--- androidApp/src/main/java/br/com/empiricus/statusviajante/android/cadastroUsuario/CadastroUsuario.kt
+++ androidApp/src/main/java/br/com/empiricus/statusviajante/android/cadastroUsuario/CadastroUsuario.kt
@@ -1,4 +1,99 @@
 package br.com.empiricus.statusviajante.android.cadastroUsuario
 
-class CadastroUsuario {
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.empiricus.statusviajante.android.MyApplicationTheme
+import br.com.empiricus.statusviajante.android.components.outLinedButtonComponent
+import br.com.empiricus.statusviajante.android.components.outLinedTextFildComponent
+import br.com.empiricus.statusviajante.android.components.topBarComponent
+import com.google.android.material.textfield.TextInputEditText
+
+
+fun cadastrarUsuario(nome : String) {
+    Log.i("Cadastrar Usuario", nome)
+}
+@Composable
+fun cadastroUsuario(onBack: () -> Boolean) {
+    MyApplicationTheme {
+        Scaffold(
+            topBar = { topBarComponent(onClickNav = { onBack.invoke() }) }
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                MaterialTheme.colors.secondaryVariant,
+                                MaterialTheme.colors.primaryVariant
+                            )
+                        )
+                    )
+                    .fillMaxSize()
+                    .padding(it),
+                verticalArrangement = Arrangement.spacedBy(25.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                item{
+                    Spacer(modifier = Modifier.height(35.dp))
+                    Text(text = "BEM-VINDO")
+
+                }
+
+                item {
+                    val nomeUsuario = remember { mutableStateOf(TextFieldValue()) }
+                    outLinedTextFildComponent(valor = nomeUsuario, title = "NOME")
+
+                }
+
+                item {
+                    val email = remember {
+                    mutableStateOf(TextFieldValue()) }
+                    outLinedTextFildComponent(valor = email, title = "EMAIL")
+                }
+
+                item {
+                    val senha = remember {
+                    mutableStateOf(TextFieldValue()) }
+                    outLinedTextFildComponent(valor = senha, title = "SENHA")
+                }
+                item {
+                    val dataNascimento = remember {
+                    mutableStateOf(TextFieldValue())
+                    }
+                    outLinedTextFildComponent(valor = dataNascimento, title = "DATA NASCIMENTO")
+                }
+                item {
+                    val celular = remember {
+                    mutableStateOf(TextFieldValue()) }
+                    outLinedTextFildComponent(valor = celular, title = "CELULAR")
+                }
+                item {
+                    Spacer(modifier = Modifier.height(25.dp))
+                    outLinedButtonComponent(onNavigationIconClick = {  cadastrarUsuario("CADASTRO") }, title = "Cadastrar usuário")
+                }
+            }
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun loginPreview() {
+    cadastroUsuario(onBack = { true })
 }
