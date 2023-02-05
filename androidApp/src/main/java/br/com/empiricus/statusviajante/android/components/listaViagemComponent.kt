1f46ff9dc126cce69b5c853af8ecd2fbf505a43c
Thiago Da Silva Barbosa Camelo <thiagocamelo1999@gmail.com>
2023-02-05 15:24:24 -0300

    Criação da tela de Listas de viagem

---
+++ androidApp/src/main/java/br/com/empiricus/statusviajante/android/components/listaViagemComponent.kt
@@ -0,0 +1,110 @@
+package br.com.empiricus.statusviajante.android.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@JvmOverloads
+@Composable
+fun listaViagemComponent(
+    onItemClick: () -> Unit = {},
+    id: Long,
+    title: String,
+    dataIda: String,
+    dataVolta: String
+) {
+    Row() {
+        Card(
+            modifier = Modifier.padding(bottom = 0.5.dp)
+
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp)
+                    .clickable { onItemClick }
+            ) {
+                Column() {
+                    Text(
+                        text =
+                        title,
+                        color = MaterialTheme.colors.primary,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
+
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(0.9f)
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxWidth(0.5f),
+                            horizontalAlignment = Alignment.Start
+                        ) {
+                            Text(
+                                text = "Data - Ida",
+                                color = MaterialTheme.colors.primary,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 12.sp
+                            )
+                        }
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.End
+                        ) {
+                            Text(
+                                text = "Data - Volta",
+                                color = MaterialTheme.colors.primary,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 12.sp
+                            )
+                        }
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth(0.9f)
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxWidth(0.5f),
+                            horizontalAlignment = Alignment.Start
+                        ) {
+                            Text(
+                                text = dataIda,
+                                color = MaterialTheme.colors.primary,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 12.sp
+                            )
+                        }
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.End
+                        ) {
+                            Text(
+                                text = dataVolta,
+                                color = MaterialTheme.colors.primary,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 12.sp
+                            )
+                        }
+                    }
+                }
+
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
+    }
+}
