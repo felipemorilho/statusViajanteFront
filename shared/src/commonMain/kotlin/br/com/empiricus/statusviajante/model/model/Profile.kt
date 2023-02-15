58bb67984e757e5d0238150626173545bf5cbbb3
ludmilact <ludmilatimoteo@Outlook.com>
2023-02-15 13:48:01 -0300

    ajustes para realizar requisicao.

--- shared/src/commonMain/kotlin/br/com/empiricus/statusviajante/model/model/Profile.kt
+++ shared/src/commonMain/kotlin/br/com/empiricus/statusviajante/model/model/Profile.kt
@@ -1,7 +1,7 @@
 package br.com.empiricus.statusviajante.model.model
 
 @kotlinx.serialization.Serializable
-class ProfileToken(val token: String, val user: Profile)
+class ProfileToken(val token: String)
 
 @kotlinx.serialization.Serializable
 class Profile(val name: String, val email: String, val photo: String)
